25a655e4187adae7b0a894e81f0ba0505d328449
Josh Schonstal <josh@schonstal.org>
2011-04-30 01:17:02 -0700

    Remove debug ray

--- Assets/Platform/Pointer.js
+++ Assets/Platform/Pointer.js
@@ -4,7 +4,6 @@ Screen.showCursor = false;
 
 function LateUpdate () {
 	var ray = camera.ScreenPointToRay (Input.mousePosition);
-	Debug.DrawRay (ray.origin, ray.direction * 20, Color.red); // (Debug.DrawRay)this is for visual confirmation and can be commented out for build.
 	var target = ray.GetPoint(-ray.origin.z / ray.direction.z); 
 	object.transform.position = target;
 }
