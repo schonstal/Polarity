e70b2e8328180ce225208951bd5db158e19351a6
Josh Schonstal <josh@schonstal.org>
2011-04-30 01:40:42 -0700

    Bouding box for camera

--- Assets/Platform/FollowObject.js
+++ Assets/Platform/FollowObject.js
@@ -2,12 +2,21 @@ var target : Transform;
 var followX = true;
 var followY = false;
 
+//Kinda gross, but I like the way it shows up in the editor...
+var minimumX = -100;
+var maximumX = 100;
+var minimumY = -100;
+var maximumY = 100;
+
 function Update() {
-	//Remember: Add in ability to have a bounding box
     if(target) {
-		if(followX)
-			transform.position.x = target.position.x;
-		if(followY)
-			transform.position.y = target.position.y;
+		if(followX) {
+			if(target.position.x > minimumX && target.position.x < maximumX)
+				transform.position.x = target.position.x;
+		}
+		if(followY) {
+			if(target.position.y > minimumY && target.position.y < maximumY)
+				transform.position.y = target.position.y;
+		}
     }
 }
