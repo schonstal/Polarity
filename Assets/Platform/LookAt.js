7611322eb860434b3914f67ce8eac59185c98048
Josh Schonstal <josh@schonstal.org>
2011-04-30 01:51:13 -0700

    Fix gross formatting on LookAt.js

--- Assets/Platform/LookAt.js
+++ Assets/Platform/LookAt.js
@@ -1,19 +1,14 @@
 var target : Transform;
 function Update ()
 {
-
-
-		var hit : RaycastHit;
-		
-		if (Physics.Raycast (transform.position, Vector3.Normalize(target.position - transform.position), hit)) {
-			if(Input.GetMouseButtonDown(0)) {
-				hit.transform.renderer.material.color = Color.red;
-			}
-			if(Input.GetMouseButtonDown(1)) {
-				hit.transform.renderer.material.color = Color.blue;
-			}
-		}
-
+	var hit : RaycastHit;
 	
-	//transform.LookAt(Vector3(target.position.x, target.position.y, transform.position.z)); 
+	if (Physics.Raycast (transform.position, Vector3.Normalize(target.position - transform.position), hit)) {
+		if(Input.GetMouseButtonDown(0)) {
+			hit.transform.renderer.material.color = Color.red;
+		}
+		if(Input.GetMouseButtonDown(1)) {
+			hit.transform.renderer.material.color = Color.blue;
+		}
+	}
 }
