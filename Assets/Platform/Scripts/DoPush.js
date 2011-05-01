734ce2033474fcfb8d98fc781f6ca91bffc735cb
Josh Schonstal <josh@schonstal.org>
2011-04-30 18:38:41 -0700

    Camera Tweaks

--- Assets/Platform/Scripts/DoPush.js
+++ Assets/Platform/Scripts/DoPush.js
@@ -1,7 +1,8 @@
 var direction = 1;
+var amount = 20;
 
 function OnTriggerStay (other : Collider) {
     if (other.attachedRigidbody) {
-        other.attachedRigidbody.AddForce(transform.up * 20 * direction * transform.parent.GetComponent(Polarity).polarity);
+        other.attachedRigidbody.AddForce(transform.up * amount * direction * transform.parent.GetComponent(Polarity).polarity);
     }
 }
