e2cce9ad816bff5bfa0ae4a806c2f2d66d07a773
Josh Schonstal <josh@schonstal.org>
2011-05-02 14:42:54 -0700

    Lights fade out

--- Assets/Platform/Scripts/LightPolarity.js
+++ Assets/Platform/Scripts/LightPolarity.js
@@ -2,31 +2,40 @@ var repulsionColor = Color.red;
 var attractionColor = Color.blue;
 var maxIntensity = 1.0;
 var lerpTime = 0.1;
+var lerpFadeTime = 0.2;
 
 private var intensity : float = 0.0;
 private var lerpCounter : float = 0.0;
+private var lerpFadeCounter : float = 0.0;
 
 function FixedUpdate() {
 	var light : Light = gameObject.GetComponent(Light);
 	var polarity : int = transform.parent.GetComponent(Polarity).polarity;
-	
-	if(lerpCounter < 1/lerpTime) {
-		lerpCounter += Time.deltaTime * 1/lerpTime;
-	}
+
 	
 	switch(polarity) {
 		case 1:
-			intensity = Mathf.Lerp(0, maxIntensity, lerpCounter);
-			light.color = repulsionColor;
+			setLightColor(repulsionColor);
 			break;
 		case -1:
-			intensity = Mathf.Lerp(0, maxIntensity, lerpCounter);
-			light.color = attractionColor;
+			setLightColor(attractionColor);
 			break;
 		default:
-			intensity = 0;
+			if(lerpFadeCounter < 1) {
+				lerpFadeCounter += Time.deltaTime * 1/lerpFadeTime;
+			}
+			intensity = Mathf.Lerp(maxIntensity, 0, lerpFadeCounter);
 			lerpCounter = 0;
 	}
 	
 	light.intensity = intensity;
+}
+
+function setLightColor(color : Color) {
+	if(lerpCounter < 1) {
+		lerpCounter += Time.deltaTime * 1/lerpTime;
+	}
+	intensity = Mathf.Lerp(0, maxIntensity, lerpCounter);
+	light.color = color;
+	lerpFadeCounter = 0;
 }
