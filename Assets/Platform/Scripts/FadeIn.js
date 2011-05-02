af126610bf216435ab909fe0389a33c03a6d2872
Josh Schonstal <josh@schonstal.org>
2011-05-02 02:13:25 -0700

    nice fades

--- Assets/Platform/Scripts/FadeIn.js
+++ Assets/Platform/Scripts/FadeIn.js
@@ -2,6 +2,8 @@ private var cameraFade : CameraFade;
 var fadeTime = 0.4;
 
 //Awake
+function Start() {
 cameraFade = gameObject.AddComponent(CameraFade);
 cameraFade.SetScreenOverlayColor(Color.black);
 cameraFade.StartFade(Color.clear, fadeTime);
+}
--- Assets/Platform/Scripts/Win.js
+++ Assets/Platform/Scripts/Win.js
@@ -1,6 +1,15 @@
+var fadeTime = 0.7;
+private var cameraFade : CameraFade;
+
 function OnTriggerEnter (other : Collider) {
 	var nextLevel = Application.loadedLevel + 1;
 	if(nextLevel < Application.levelCount) {
+		cameraFade = gameObject.AddComponent(CameraFade);
+		cameraFade.StartFade(Color.black, fadeTime);
+		
+		yield WaitForSeconds(0.6);
+    	
     	Application.LoadLevel(nextLevel);
     }
 }
+
