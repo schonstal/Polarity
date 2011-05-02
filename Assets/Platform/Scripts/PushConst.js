9fff0250b00ee38dd97043cb7ae2ca59a19e8434
Ian Brock <pibborando@gmail.com>
2011-05-02 09:47:32 -0700

    added particles and light to gravity plate.  added trail to player.  added force at entrance.

---
+++ Assets/Platform/Scripts/PushConst.js
@@ -0,0 +1,8 @@
+var direction = 1;
+var amount = 20;
+
+function OnTriggerStay (other : Collider) {
+    if (other.attachedRigidbody) {
+        other.attachedRigidbody.AddForce(transform.up * amount * direction);
+    }
+}
